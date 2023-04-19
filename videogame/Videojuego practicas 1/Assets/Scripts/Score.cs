d7f9c417d0b3bc96431df1683f8639ed6af431c0
Santiago Benitez <A01782813@tec.mx>
2023-04-19 15:26:01 -0600

    added score functionality to basketball game

---
+++ videogame/Videojuego practicas 1/Assets/Scripts/Score.cs
@@ -0,0 +1,29 @@
+/*Script to track points scored */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] ParticleSystem starParticles; 
+    //[SerializeField] GameObject particles;
+    [SerializeField] TMP_Text scoreText;
+    int points;
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = 0;
+    }
+ 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        points += 1;
+        print("Points: " + points);
+        scoreText.text = "Score: " + points;
+        starParticles.Emit(15); 
+        //Instantiate(particles, transform.position, Quaternion.identity);
+    } 
+}
